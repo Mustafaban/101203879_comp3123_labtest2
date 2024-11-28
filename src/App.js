d84c9ea64fdbb58dcf5c6eb0d488de615e0e1c1e
MustafaBan <bandukdamustafa1@gmail.com>
2024-11-29 00:01:22 +0700

    updated app js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Header from "./components/Header";
+import SearchBar from "./components/Search";
+import WeatherInfo from "./components/Weather";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [city, setCity] = useState("Toronto");
+  const [weather, setWeather] = useState(null);
+  const [error, setError] = useState(null);
+
+  const fetchWeather = async () => {
+    try {
+      const response = await axios.get(
+        `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=16844e72420d6be36afefcacbd484518&units=metric`
+      );
+      setWeather(response.data);
+      setError(null);
+    } catch (err) {
+      setError("City not found. Please try again.");
+      setWeather(null);
+    }
+  };
+
+  useEffect(() => {
+    fetchWeather();
+  }, [city]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="weather-app">
+      <Header />
+      <SearchBar city={city} setCity={setCity} fetchWeather={fetchWeather} />
+      <WeatherInfo weather={weather} error={error} />
     </div>
   );
-}
+};
 
 export default App;
