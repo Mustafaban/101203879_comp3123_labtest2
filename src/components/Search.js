054745a336e4542d5c06434c1d52de74c3dea550
MustafaBan <bandukdamustafa1@gmail.com>
2024-11-29 00:42:04 +0700

    materialui add to search

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,16 +1,26 @@
 import React from "react";
+import { TextField, Button, Box } from "@mui/material";
 
 const Search = ({ city, setCity, fetchWeather }) => {
   return (
-    <div className="search-bar">
-      <input
-        type="text"
+    <Box display="flex" justifyContent="center" my={3}>
+      <TextField
+        label="Enter City"
+        variant="outlined"
         value={city}
         onChange={(e) => setCity(e.target.value)}
-        placeholder="Enter City"
+        sx={{ mr: 2, backgroundColor: "#ffffff", borderRadius: "5px" }}
+        fullWidth
       />
-      <button onClick={fetchWeather}>Search</button>
-    </div>
+      <Button
+        variant="contained"
+        color="primary"
+        onClick={fetchWeather}
+        className="search-button"
+      >
+        Search
+      </Button>
+    </Box>
   );
 };
 
